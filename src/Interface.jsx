b6e238344d91e35aeb2cc8f5593d9d9152585da8
brunosimon <simon.bruno.77@gmail.com>
2023-01-02 22:33:51 +0000

    :sparkles: Interface > Add arrow

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -3,8 +3,20 @@ import useStore from './stores/useStore'
 export default function Interface()
 {
     const index = useStore(state => state.index)
-    // console.log(index)
+    const models = useStore(state => state.models)
+    const previous = useStore(state => state.previous)
+    const next = useStore(state => state.next)
+
     return <>
-        <button>next</button>
+        <button className={ `button is-previous ${ index > 0 ? 'is-active' : '' }` } onClick={ previous }>
+            <div className="icon">
+                <div className="arrow"></div>
+            </div>
+        </button>
+        <button className={ `button is-next ${ index < models.length - 2 ? 'is-active' : '' }` } onClick={ next }>
+            <div className="icon">
+                <div className="arrow"></div>
+            </div>
+        </button>
     </>
 }
