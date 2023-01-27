1638412755c1893b810a280569334d7ed833b286
brunosimon <simon.bruno.77@gmail.com>
2023-01-27 11:44:40 +0100

    :art: Move orientation detection to Experience

--- src/Application.jsx
+++ src/Application.jsx
@@ -2,39 +2,16 @@ import { Canvas } from '@react-three/fiber'
 import Experience from './Experience'
 import { Leva } from 'leva'
 import Interface from './Interface'
-import useStore from './stores/useStore'
-import { useEffect } from 'react'
 
 export default function Application()
 {
-    const orientation = useStore(state => state.orientation)
-    const setOrientation = useStore(state => state.setOrientation)
-
-    const resize = () =>
-    {
-        const orientation = window.innerWidth < window.innerHeight ? 'portrait' : 'landscape'
-        setOrientation(orientation)
-    }
-
-    useEffect(() =>
-    {
-        window.addEventListener('resize', resize)
-
-        return () =>
-        {
-            window.removeEventListener('resize', resize)
-        }
-    }, [])
-
-    const distanceMultiplier = orientation == 'portrait' ? 2.5 : 1
-
     return <>
         <Leva collapsed />
         <Canvas
             flat
             camera={ {
                 fov: 35,
-                position: [ 2 * distanceMultiplier, 4, 6 * distanceMultiplier ]
+                position: [ 2, 4, 6 ]
             } }
         >
             <Experience />
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,12 +2,30 @@ import { useControls } from 'leva'
 import { OrbitControls } from '@react-three/drei'
 import Floor from './Floor'
 import Models from './Models'
+import useStore from './stores/useStore'
+import { useEffect } from 'react'
+import { useThree } from '@react-three/fiber'
 
 export default function Experience()
 {
     const options = useControls('general', {
         background: { value: '#171717' }
     })
+
+    const orientation = useStore(state => state.orientation)
+    const setOrientation = useStore(state => state.setOrientation)
+    const size = useThree((state) => state.size)
+    const camera = useThree((state) => state.camera)
+
+    useEffect(() =>
+    {
+        setOrientation(size.width < size.height ? 'portrait' : 'landscape')
+    }, [ size ])
+
+    useEffect(() =>
+    {
+        camera.position.multiplyScalar(orientation == 'portrait' ? 2 : 1)
+    }, [])
 
     return <>
         <color args={ [ options.background ] } attach="background" />
